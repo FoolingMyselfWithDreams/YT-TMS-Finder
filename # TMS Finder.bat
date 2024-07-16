f7f674c8b84e4b40870d1a71cde7093a04cb5e23
FoolingMyselfWithDreams <72178420+FoolingMyselfWithDreams@users.noreply.github.com>
2024-07-16 19:05:11 +0200

    Update # TMS Finder.bat

--- # TMS Finder.bat
+++ # TMS Finder.bat
@@ -1,10 +1,10 @@
 @echo off
-set Location=C:%HOMEPATH%\AppData\Local\Programs\Python\
+python check_python.py
 
 :Check to see if the Python directory exists, if not, display error message:
-IF EXIST "%Location%" ( 
+IF %ERRORLEVEL% == 10 (
+    echo This program uses Python 3.8 or newer, which is not installed on your system. Please double-click Install.bat or download and install Python at https://www.python.org/downloads/. & pause
+) ELSE (
     python find_stable.py
     cmd /k
-) ELSE (
-    echo This program uses Python, which is not installed on your system. Please double-click Install.bat or download and install Python at https://www.python.org/downloads/. & pause
 )
