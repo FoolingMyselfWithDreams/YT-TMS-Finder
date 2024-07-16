4ecb56ea891d6fad3d4996fc00aef7824f79de69
FoolingMyselfWithDreams <72178420+FoolingMyselfWithDreams@users.noreply.github.com>
2024-07-16 18:03:36 +0200

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -9,23 +9,22 @@ if %ERRORLEVEL% == 0 (
 	cmd /k
 )
 
-python --version 2>NUL
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+call RefreshEnv.cmd
 
-:Check to see if Python is in PATH, if not, install Python:
-if %ERRORLEVEL% == 9009 (
-	@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-	RefreshEnv.cmd
-	choco install python3 -y
-	RefreshEnv.cmd
-        pip install -r requirements.txt
-	choco install chromedriver -y
-	choco install ffmpeg -y
-        cmd /k
-) else (
-	@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-        RefreshEnv.cmd
-	pip install -r requirements.txt
-	choco install chromedriver -y
-	choco install ffmpeg -y
-        cmd /k
+python check_python.py
+
+:Check to see if Python version is sufficient, if not, install Python:
+if %ERRORLEVEL% == 10 (
+	choco upgrade python3 -y
+	call RefreshEnv.cmd
 )
+if %ERRORLEVEL% == 9009 (
+	choco install python3 -y
+	call RefreshEnv.cmd
+)
+
+pip install -r requirements.txt
+choco install chromedriver -y
+choco install ffmpeg -y
+cmd /k
