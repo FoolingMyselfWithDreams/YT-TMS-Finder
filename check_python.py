a5997e48345d9459b64742fd56e3b6f360c37aa9
FoolingMyselfWithDreams <72178420+FoolingMyselfWithDreams@users.noreply.github.com>
2024-07-19 18:28:14 +0200

    Update check_python.py

--- check_python.py
+++ check_python.py
@@ -2,7 +2,6 @@ import sys
 import platform
 
 def main():
-    print(sys.version_info)
     if sys.version_info[0] <= 2 or (sys.version_info[0] == 3 and sys.version_info[1] < 8):
         exit(10)
     else:
